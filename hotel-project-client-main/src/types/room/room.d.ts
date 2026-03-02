36f2bf357993912a0d016c52ba8da3e683d77610
miji <mjkim2304@gmail.com>
2026-03-02 13:30:51 +0900

    fix(room): 방 사진 관리 기능 추가

--- hotel-project-client-main/src/types/room/room.d.ts
+++ hotel-project-client-main/src/types/room/room.d.ts
@@ -5,3 +5,33 @@ export interface Room {
   maxOccupancy: number;
   price: number;
 }
+
+export interface RoomInfo {
+  hotelId: number;
+  hotelName: string;
+  roomId: number;
+  roomType: string;
+  description: string;
+  maxOccupancy: number;
+  price: number;
+  totalQuantity: number;
+  mainImageUrl?: string;
+  additionalPhotoUrls?: string[];
+}
+
+export interface CreateRoomRequest {
+  description: string;
+  roomType: string;
+  price: number;
+  occupancy: number;
+  totalQuantity: number;
+}
+
+export interface UpdateRoomRequest {
+  roomId: number;
+  description: string;
+  roomType: string;
+  maxOccupancy: number;
+  price: number;
+  totalQuantity: number;
+}
